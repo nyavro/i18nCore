58658fcfe739200b38e62c28a534e2f0418edc75
nyavro <e.nyavro@gmail.com>
2022-10-30 21:28:06 +0700

    Migration of completion

--- src/main/kotlin/com/eny/i18n/plugin/core/ide/completion/CompositeKeyCompletionContributor.kt
+++ src/main/kotlin/com/eny/i18n/plugin/core/ide/completion/CompositeKeyCompletionContributor.kt
@@ -1,14 +1,14 @@
-package com.eny.i18n.plugin.ide.completion
+package com.eny.i18n.plugin.core.ide.completion
 
 import com.eny.i18n.plugin.core.key.KeyExtractor
+import com.eny.i18n.plugin.core.key.KeyResolverService
+import com.eny.i18n.plugin.core.localization.source.LocalizationSourceService
 import com.eny.i18n.plugin.factory.CallContext
 import com.eny.i18n.plugin.ide.settings.Settings
 import com.eny.i18n.plugin.key.FullKey
 import com.eny.i18n.plugin.key.lexer.Literal
 import com.eny.i18n.plugin.parser.DummyContext
-import com.eny.i18n.plugin.tree.CompositeKeyResolver
 import com.eny.i18n.plugin.tree.PsiElementTree
-import com.eny.i18n.plugin.utils.LocalizationSourceSearch
 import com.eny.i18n.plugin.utils.nullableToList
 import com.eny.i18n.plugin.utils.unQuote
 import com.intellij.codeInsight.completion.CompletionContributor
@@ -16,19 +16,17 @@ import com.intellij.codeInsight.completion.CompletionInitializationContext
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
 import com.intellij.psi.PsiElement
 
 /**
  * Completion of i18n key
  */
-abstract class CompositeKeyCompletionContributor(private val callContext: CallContext): CompletionContributor(), CompositeKeyResolver<PsiElement> {
+abstract class CompositeKeyCompletionContributor(private val callContext: CallContext): CompletionContributor() {
     private val DUMMY_KEY = CompletionInitializationContext.DUMMY_IDENTIFIER_TRIMMED
     private val dummyContext = DummyContext()
 
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-//        super.fillCompletionVariants(parameters, result)
         if(parameters.position.text.unQuote().substringAfter(DUMMY_KEY).trim().isNotBlank()) return
         val fullKey = parameters.editor.project?.service<KeyExtractor>()?.extractFullKey(dummyContext, parameters.position)
         if (fullKey == null) {
@@ -49,13 +47,6 @@ abstract class CompositeKeyCompletionContributor(private val callContext: CallCo
         prefix, "", null, emptyList(), element
     )
 
-    private fun groupPlurals(completions: List<String>, pluralSeparator: String):List<String> =
-        completions.groupBy {it.substringBeforeLast(pluralSeparator)}
-            .entries.flatMap {
-                entry -> if(entry.value.size == 3 && entry.value.containsAll(listOf(1,2,5).map{entry.key+pluralSeparator+it})) {
-                listOf(entry.key)} else entry.value
-            }
-
     private fun processKey(fullKey: FullKey, element: PsiElement): List<LookupElementBuilder> =
         fullKey.compositeKey.lastOrNull().nullableToList().flatMap { last ->
             val source = fullKey.source.replace(last.text, "")
@@ -64,9 +55,10 @@ abstract class CompositeKeyCompletionContributor(private val callContext: CallCo
         }
 
     private fun findCompletions(prefix: String, source: String, ns: String?, compositeKey: List<Literal>, element: PsiElement): List<LookupElementBuilder> {
-        return groupPlurals(
-            LocalizationSourceSearch(element.project).findSources(ns.nullableToList(), element).flatMap {
-                listCompositeKeyVariants(
+        val keyResolverService = element.project.service<KeyResolverService>()
+        return keyResolverService.groupPlurals(
+            element.project.service<LocalizationSourceService>().findSources(ns.nullableToList(), element).flatMap {
+                keyResolverService.listCompositeKeyVariants(
                     compositeKey,
                     PsiElementTree.create(it.element),
                     prefix,
--- src/main/kotlin/com/eny/i18n/plugin/core/key/KeyResolverService.kt
+++ src/main/kotlin/com/eny/i18n/plugin/core/key/KeyResolverService.kt
@@ -5,4 +5,13 @@ import com.intellij.openapi.components.Service
 import com.intellij.psi.PsiElement
 
 @Service
-class KeyResolverService: CompositeKeyResolver<PsiElement>
+class KeyResolverService: CompositeKeyResolver<PsiElement> {
+
+    fun groupPlurals(completions: List<String>, pluralSeparator: String):List<String> =
+        completions.groupBy {it.substringBeforeLast(pluralSeparator)}
+            //TODO: technology specific
+            .entries.flatMap {
+                    entry -> if(entry.value.size == 3 && entry.value.containsAll(listOf(1,2,5).map{entry.key+pluralSeparator+it})) {
+                listOf(entry.key)} else entry.value
+            }
+}
